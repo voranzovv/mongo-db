73c65fe1c54e9a98b8419eb2b90f95abed34c47a
voranzovv <voranzov@gmail.com>
2019-12-24 17:31:54 +1100

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err=> console.error("Could not connect to DB", err));
+
+  const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type: Date, default:Date.now}
+  })
